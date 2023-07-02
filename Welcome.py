f7e34fecec03f228337bab0192ce74df76e282a5
Ajay Mahajan <ajmahajan109@gmail.com>
2023-07-02 12:54:39 +0530

    ajay

---
+++ Welcome.py
@@ -0,0 +1,63 @@
+from tkinter import *
+import login
+class WelcomeWindow:
+    #create a constructor
+    def _init_(self):
+        # create a tkinter window
+        self.win = Tk()
+
+        #reset the window and background color
+        self.canvas = Canvas(self.win, width=600, height=400, bg='white')
+        self.canvas.pack(expand=YES, fill=BOTH)
+
+        #show window in center of the screen
+        width = self.win.winfo_screenwidth()
+        height = self.win.winfo_screenheight()
+        x = int(width / 2 - 600 / 2)
+        y = int(height / 2 - 400 / 2)
+        str1 = "600x400+"+ str(x) + "+" + str(y)
+        self.win.geometry(str1)
+
+        #disable resize of the window
+        self.win.resizable(width=False, height=False)
+
+        #change the title of the window
+        self.win.title("WELCOME | PROJECT TITLE | ADMINISTRATOR")
+
+    def add_frame(self):
+        #create a inner frame
+        self.frame = Frame(self.win, height=300, width=450)
+        self.frame.place(x=80, y=50)
+
+        x, y = 70, 20
+
+        # place the photo in the frame
+
+        self.img = PhotoImage(file='icon.png')
+        self.label = Label(self.frame, image=self.img)
+        self.label.place(x=x+45, y=y-30)
+
+        self.labeltitle = Label(self.frame, text="Welcome to Social distancing detector")
+        self.labeltitle.config(font=("Courier", 14, 'bold'))
+        self.labeltitle.place(x=10, y=y+150)
+
+        self.button = Button(self.frame, text="Continue", font=('helvetica', 20, 'underline italic')
+                             , bg='dark green', fg='white', command=self.login)
+        self.button.place(x=x+80, y=y+200)
+
+        self.win.mainloop()
+
+    #open a new window on button press
+    def login(self):
+        # destroy current window
+        self.win.destroy()
+
+        #open the new window
+        log = login.LoginWindow()
+        log.add_frame()
+
+
+if _name_ == "_main_":
+    x = WelcomeWindow()
+    x.add_frame()
+
---
+++ main.py
@@ -0,0 +1,7 @@
+def code1():
+    import social_distancing
+
+def code2():
+    import final
+code1()
+code2()
